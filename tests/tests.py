9f8b60de85e74ca4922a3fde260c9077cca40391
Nicolas Delon <nicolas.delon@gmail.com>
2021-08-11 16:50:31 +0200

    Add more tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,6 @@
 import re
 
 import pytest
-from pytest_mock import MockFixture
 import requests_mock
 from callee import Regex
 
@@ -40,7 +39,7 @@ def lcc_mock(mocker):
 def mock_session(session, **kwargs):
     adapter = requests_mock.Adapter()
     adapter.register_uri(requests_mock.ANY, requests_mock.ANY, **kwargs)
-    session.mount('mock://', adapter)
+    session.mount('http://', adapter)
     return session
 
 
@@ -51,16 +50,94 @@ def assert_logs(mock, *expected):
         if not isinstance(val, Regex):
             val = Regex(val)
         mock.log_info.assert_any_call(val)
+    assert len(mock.log_info.mock_calls) == len(expected)
 
 
-def test_session_get(lcc_mock):
+def test_session_default(lcc_mock):
     session = mock_session(Session(), status_code=201, headers={"Foo": "bar"}, text="foobar")
-    session.get("mock://www.example.net")
+    session.get("http://www.example.net")
     assert_logs(
         lcc_mock,
-        "HTTP request.+GET mock://www.example.net",
+        r"HTTP request.+GET http://www\.example\.net",
         "HTTP request headers",
         r".+status.+201",
         "HTTP response headers.+foo.+bar",
         "HTTP response body.+foobar"
+    )
+
+
+def test_session_log_only_request_line(lcc_mock):
+    session = mock_session(Session(), status_code=201, headers={"Foo": "bar"}, text="foobar")
+    logger = Logger.off()
+    logger.request_line_logging = True
+    session.get("http://www.example.net", logger=logger)
+    assert_logs(
+        lcc_mock,
+        r"HTTP request.+GET http://www\.example\.net",
+    )
+
+
+def test_session_log_only_request_header(lcc_mock):
+    session = mock_session(Session(), status_code=201, headers={"Foo": "bar"}, text="foobar")
+    logger = Logger.off()
+    logger.request_headers_logging = True
+    session.get("http://www.example.net", logger=logger)
+    assert_logs(
+        lcc_mock,
+        "HTTP request headers"
+    )
+
+
+def test_session_log_only_request_body_json(lcc_mock):
+    session = mock_session(Session(), status_code=201, headers={"Foo": "bar"}, text="foobar")
+    logger = Logger.off()
+    logger.request_body_logging = True
+    session.get("http://www.example.net", json={"foo": "bar"}, logger=logger)
+    assert_logs(
+        lcc_mock,
+        "HTTP request body.+JSON.+foo.+bar"
+    )
+
+
+def test_session_log_only_request_body_data_dict(lcc_mock):
+    session = mock_session(Session(), status_code=201, headers={"Foo": "bar"}, text="foobar")
+    logger = Logger.off()
+    logger.request_body_logging = True
+    session.get("http://www.example.net", data={"foo": "bar"}, logger=logger)
+    assert_logs(
+        lcc_mock,
+        "HTTP request body.+foo.+bar"
+    )
+
+
+def test_session_log_only_request_body_data_raw(lcc_mock):
+    session = mock_session(Session(), status_code=201, headers={"Foo": "bar"}, text="foobar")
+    logger = Logger.off()
+    logger.request_body_logging = True
+    session.get("http://www.example.net", data="foobar", logger=logger)
+    assert_logs(
+        lcc_mock,
+        "HTTP request body.+foobar"
+    )
+
+
+def test_session_log_only_request_body_files_as_list(lcc_mock):
+    session = mock_session(Session(), status_code=201, headers={"Foo": "bar"}, text="foobar")
+    logger = Logger.off()
+    logger.request_body_logging = True
+    session.get("http://www.example.net", files=[("file", ("plain.txt", "sometextdata", "text/plain"))], logger=logger)
+    assert_logs(
+        lcc_mock,
+        r"HTTP request body.+plain\.txt.+text/plain"
+    )
+
+
+def test_session_log_only_request_body_files_as_dict(lcc_mock):
+    session = mock_session(Session(), status_code=201, headers={"Foo": "bar"}, text="foobar")
+    logger = Logger.off()
+    logger.request_body_logging = True
+    session.get("http://www.example.net", files={"file": ("plain.txt", "sometextdata", "text/plain")}, logger=logger)
+    assert_logs(
+        lcc_mock,
+        r"HTTP request body.+plain\.txt.+text/plain"
     )
