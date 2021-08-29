d602d64afc555f56d3f1e449b8edd29b377d215c
Nicolas Delon <nicolas.delon@gmail.com>
2021-08-29 14:33:27 +0200

    doc: get rid of off auto-generated comments in conf.py to make this file more readable

--- doc/conf.py
+++ doc/conf.py
@@ -1,26 +1,8 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
 import sys
 sys.path.append("..")
 
 from lemoncheesecake_requests.__version__ import __version__
 
-
-# -- Project information -----------------------------------------------------
 
 project = 'lemoncheesecake-requests'
 copyright = '2021, Nicolas Delon'
@@ -28,48 +10,30 @@ author = 'Nicolas Delon'
 version = __version__
 release = __version__
 
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx"
 ]
 
 autodoc_typehints = "description"
-
 autodoc_default_options = {
     'member-order': 'bysource'
 }
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
 # This is not actually the root doc (the root doc is "index") but we need
 # so that Sphinx knows about the toc
 root_doc = "toc"
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = 'alabaster'
-
 html_theme_options = {
     "page_width": "1150px",
     "sidebar_width": "300px",
     "fixed_sidebar": True
 }
-
 html_sidebars = {
     '**': [
         'logo.html',
@@ -79,12 +43,7 @@ html_sidebars = {
         'searchbox.html',
     ]
 }
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
 html_show_sourcelink = False
 
 intersphinx_mapping = {
