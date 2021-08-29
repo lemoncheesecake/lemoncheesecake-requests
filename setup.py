a7e867f157c1c3e531fa0a3d66d324c063a874ff
Nicolas Delon <nicolas.delon@gmail.com>
2021-08-29 15:07:56 +0200

    setup.py: various changes (finalization work)

--- setup.py
+++ setup.py
@@ -4,23 +4,23 @@ import os.path as osp
 from setuptools import setup, find_packages
 
 
-current_dir = osp.abspath(osp.dirname(__file__))
+top_dir = osp.abspath(osp.dirname(__file__))
 
 info = {}
-with open(osp.join(current_dir, "lemoncheesecake_requests", "__version__.py")) as fh:
+with open(osp.join(top_dir, "lemoncheesecake_requests", "__version__.py")) as fh:
     exec(fh.read(), info)
 
 setup(
     name="lemoncheesecake-requests",
     version=info["__version__"],
-    description="Test Storytelling",
-    # long_description=open("README.rst").read(),
+    description="Test Storytelling for requests",
+    long_description=open(osp.join(top_dir, "README.rst")).read(),
     author="Nicolas Delon",
     author_email="nicolas.delon@gmail.com",
     license="Apache License (Version 2.0)",
-    # url="http://lemoncheesecake.io",
+    url="https://lemoncheesecake-requests.readthedocs.io",
     classifiers=[
-        # "Development Status :: 5 - Production/Stable",
+        "Development Status :: 4 - Beta"
         "Environment :: Console",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
@@ -35,14 +35,13 @@ setup(
         "Topic :: Software Development :: Quality Assurance",
         "Topic :: Software Development :: Testing",
     ],
-    keywords="QA testing requests",
+    keywords="QA testing lemoncheesecake requests",
     project_urls={
-        # 'Documentation': 'http://docs.lemoncheesecake.io/',
-        # 'Source': 'https://github.com/lemoncheesecake/lemoncheesecake',
-        # 'Tracker': 'https://github.com/lemoncheesecake/lemoncheesecake/issues',
+        'Documentation': 'https://lemoncheesecake-requests.readthedocs.io.',
+        'Source': 'https://github.com/lemoncheesecake/lemoncheesecake-requests',
+        'Tracker': 'https://github.com/lemoncheesecake/lemoncheesecake-requests/issues',
     },
 
     packages=find_packages(),
-    include_package_data=True,
-    install_requires=("lemoncheesecake~=1.6", "requests~=2.23.0"),
+    install_requires=("lemoncheesecake~=1.6", "requests~=2.23.0")
 )
