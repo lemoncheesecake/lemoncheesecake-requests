91ccff0f50c0716cc07e47b8fefb7f63d291ef04
Nicolas Delon <nicolas.delon@gmail.com>
2021-08-29 12:41:54 +0200

    doc: fix left menu

--- doc/conf.py
+++ doc/conf.py
@@ -63,6 +63,15 @@ html_theme_options = {
     "sidebar_width": "300px",
 }
 
+html_sidebars = {
+    '**': [
+        'logo.html',
+        'relations.html',  # needs 'show_related': True theme option to display
+        'navigation.html',
+        'searchbox.html',
+    ]
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
